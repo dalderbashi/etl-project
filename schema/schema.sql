1e4e50b2b799f2e175d81c2dbc77da884c0f20e8
Eshun <IEshun@NA.AMFBowl.NET>
2019-10-16 21:23:53 -0400

    this is the final Im sure

--- schema/schema.sql
+++ schema/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE soybean_transformed(
 
 CREATE TABLE crude_oil_transformed(
 	Date DATE NOT NULL,
-	Crude_open_Price FLOAT NOT NULL,
+	Crude_Open_Price FLOAT NOT NULL,
 	Crude_Avg_Price FLOAT NOT NULL,
 	PRIMARY KEY (Date)
 );		
