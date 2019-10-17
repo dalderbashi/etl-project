10b3fa03360f2c0c3532bbef5e9166f3ae15eaa1
wavyhair3 <diana.alderbashi@richmond.edu>
2019-10-16 22:01:54 -0400

    load.py

---
+++ src/load.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+from sqlalchemy import create_engine
+import datetime as dt
+
+connection_string = "postgres:postgres@localhost:5432/soybeans_trade"
+engine = create_engine(f'postgresql://{connection_string}')
+
+#Confirm tables
+print(engine.table_names())
+
+
+
+soybean_file = "./output/soybean_transformed.csv"
+crude_oil_file = "./output/crude_oil_transformed.csv"
+iowa_file = "./output/iowa_transformed.csv"
+illinois_file = "./output/illinois_transformed.csv"
+
+#convert csv to data frame and convert column values to float
+soybean_df = pd.read_csv(soybean_file)
+crude_oil_df = pd.read_csv(crude_oil_file)
+iowa_df = pd.read_csv(iowa_file)
+illinois_df = pd.read_csv(illinois_file)
+
+
+soybean_df.to_sql(name='soybean_transformed', con=engine, if_exists='append', index=False)
+crude_oil_df.to_sql(name='crude_oil_transformed', con=engine, if_exists='append', index=False)
+iowa_df.to_sql(name='iowa_transformed', con=engine, if_exists='append', index=False)
+illinois_df.to_sql(name='illinois_transformed', con=engine, if_exists='append', index=False)
+
+print(soybean_df.head())
---
+++ src/transforms/soybeans.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env pytho
+# coding : utf - 8
+
+## Load and Transform Soybean Data
+
+import pandas as pd
+from sqlalchemy import create_engine
+import datetime as dt
+import os
+
+#home_dir = os.getcwd()
+#soybean_file_test_path = os.path.join(home_dir, '/input/soybean/US Soybeans Futures Historical Data.csv')
+
+### Extract CSVs into DataFrames
+
+soybean_file = "./input/soybean/US Soybeans Futures Historical Data.csv"
+#convert csv to data frame and convert column values to float
+soybean_df = pd.read_csv(soybean_file, thousands = ',', decimal = '.')
+#soybean_df.head()
+
+
+### Transform Soybean DataFrame
+
+#convert date from object to a date
+soybean_df['Date'] = pd.to_datetime(soybean_df['Date'], format = '%d-%b-%y')
+
+#soybean_df.dtypes
+#calculate average soybean price and add it as a new column sing high and low values
+soybean_df["soybean_avg_price"] = soybean_df[["High", "Low"]].mean(axis = 1)
+#soybean_df.head()
+
+# Create a filtered dataframe from specific columns
+soybean_cols = ["Date", "Open", "soybean_avg_price"]
+soybean_transformed = soybean_df[soybean_cols].copy()
+
+# Rename the column headers
+soybean_transformed = soybean_transformed.rename(columns = {
+    "Open": "Soybean_Open_Price",
+    "soybean_avg_price": "Soybean_Avg_Price"
+})
+#soybean_transformed.count()
+#soybean_transformed.head()
+
+
+#export to csv
+soybean_transformed.to_csv("./output/soybean_transformed.csv", index = False)
