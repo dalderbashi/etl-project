8cc0f56d2885a8b0cae2fefc5a883783dedd5851
Eshun <IEshun@NA.AMFBowl.NET>
2019-10-16 20:41:49 -0400

    updated

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,2 +1,39 @@
-CREATE DATABASE...
-CREATE TABLE...
+CREATE TABLE illinois_transformed(
+	Date DATE NOT NULL,
+	Illinois_Precip FLOAT NOT NULL,
+	Illinois_Temp FLOAT NOT NULL,
+	PRIMARY KEY (Date)
+);
+
+
+CREATE TABLE iowa_transformed(
+	Date DATE NOT NULL,
+	Iowa_Precip FLOAT NOT NULL,
+	Iowa_Temp FLOAT NOT NULL,
+	PRIMARY KEY (Date)
+);	
+
+
+CREATE TABLE soybean_transformed(
+	Date DATE NOT NULL,
+	Soybean_Open_Price FLOAT NOT NULL,
+	Soybean_Avg_Price FLOAT NOT NULL,
+	PRIMARY KEY (Date)
+);	
+
+
+CREATE TABLE crude_oil_transformed(
+	Date DATE NOT NULL,
+	Crude_open_Price FLOAT NOT NULL,
+	Crude_Avg_Price FLOAT NOT NULL,
+	PRIMARY KEY (Date)
+);		
+
+
+CREATE TABLE crude_oil_transformed(
+	Date DATE NOT NULL,
+	crude_open_price FLOAT() NOT NULL,
+	crudeoil_avg_price FLOAT() NOT NULL,
+	PRIMARY KEY (Date)
+);	
+	
